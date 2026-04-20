2d2fb085369b766ca5d448d325a2cab703df44aa
robertmak <33355199+robertmak@users.noreply.github.com>
2026-04-20 18:00:52 +0300

    Update embedscript.js
    Refactor iframe resizing logic and improve styling.

--- embedscript.js
+++ embedscript.js
@@ -8,25 +8,51 @@
   function resizeIframes() {
     var iframes = getStartupIframes();
     iframes.forEach(function(iframe) {
-      var width = iframe.parentElement.offsetWidth;
+      var parent = iframe.parentElement;
+      var width = parent ? parent.offsetWidth : iframe.offsetWidth;
 
       if (width <= 480) {
         iframe.style.height = '520px';
       } else if (width <= 768) {
         iframe.style.height = '460px';
       } else {
-        iframe.style.height = '350px';
+        iframe.style.height = '300px';
       }
 
-      // Remove scrollbar
       iframe.setAttribute('scrolling', 'no');
       iframe.style.overflow = 'hidden';
+      iframe.style.display = 'block';
+      iframe.style.border = 'none';
+      iframe.style.margin = '0';
+      iframe.style.padding = '0';
+      iframe.style.lineHeight = '0';
+
+      if (parent) {
+        parent.style.lineHeight = '0';
+        parent.style.fontSize = '0';
+        parent.style.margin = '0';
+        parent.style.padding = '0';
+      }
     });
   }
 
   window.addEventListener('message', function(event) {
     if (event.data && event.data.type === 'startups-ne-resize') {
-      var iframe = document.getElementById(event.data.id);
+      var iframe;
+      if (event.data.id) {
+        iframe = document.getElementById(event.data.id);
+      }
+      if (!iframe) {
+        var iframes = getStartupIframes();
+        for (var i = 0; i < iframes.length; i++) {
+          try {
+            if (iframes[i].contentWindow === event.source) {
+              iframe = iframes[i];
+              break;
+            }
+          } catch (err) {}
+        }
+      }
       if (iframe && event.data.height) {
         iframe.style.height = event.data.height + 'px';
       }
